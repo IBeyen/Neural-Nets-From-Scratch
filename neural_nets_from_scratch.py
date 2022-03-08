aeee4285cc7467c1c4f9feb30d7eb110851a9b3a
Ilan Beyen <ilanbeyen@gmail.com>
2022-03-07 22:40:58 -0500

    Layer code finished

--- neural_nets_from_scratch.py
+++ neural_nets_from_scratch.py
@@ -1,25 +1,22 @@
 import numpy as np
 
 
-def Linear(x, dir='forward'):
-    if dir == "backward":
-        return 1
-    else:
-        return x
 class Layer:
     # Takes number of inputs from previous layer and number of neurons for this layer
-    def __init__(self, n_input, n_neurons, activation=Linear()):
+    def __init__(self, n_input, n_neurons, activation='linear'):
         self.weights = np.random.randn(n_input+1, n_neurons)
         self.X = 0
         self.activation = activation
 
     def forward(self, x):
         self.X = np.insert(x, 0, 1)
-        return np.dot(x, self.weights)
+        if self.activation == 'linear':
+            return np.dot(x, self.weights)
 
     def backward(self, previous_layer_derivative, learning_rate=10e-3):
-        self.weights += np.dot(np.dot(previous_layer_derivative, self.activation(self.X, 'backward')), self.X)
-        return np.dot(np.dot(previous_layer_derivative, self.activation(self.X, 'backward')), self.weights)
+        if self.activation == 'linear':
+            self.weights += np.dot(previous_layer_derivative, self.X)
+            return np.dot(previous_layer_derivative, self.weights)
 
 
 class Network:
