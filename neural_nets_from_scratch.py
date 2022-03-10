dad52341536fd2996e1eee7779b114ae7871d20b
IBeyen <ilanbeyen@gmail.com>
2022-03-10 15:07:00 -0500

    Working activations

--- neural_nets_from_scratch.py
+++ neural_nets_from_scratch.py
@@ -1,9 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+class Linear:
+    def __init__(self):
+        self.X = 0
+    
+    def forward(self, X):
+        self.X = X
+        return self.X
+    
+    def backward(self):
+        return 1
+class Sigmoid:
+    def __init__(self):
+        self.X = 0
+    
+    def forward(self, X):
+        self.X = X
+        return 1/(1+np.exp(-self.X))
+    
+    def backward(self):
+        return (1/(1+np.exp(-self.X)))*(1-(1/(1+np.exp(-(self.X)))))
+
 class Layer:
     # Takes number of inputs from previous layer and number of neurons for this layer
-    def __init__(self, n_input, n_neurons, activation='linear'):
+    def __init__(self, n_input, n_neurons, activation=Linear()):
         self.weights = np.random.randn(n_input+1, n_neurons)
         self.X = 0
         self.activation = activation
@@ -11,13 +32,12 @@ class Layer:
     def forward(self, x):
         #x=examples, inputs    w=number of inputs, neurons
         self.X = np.concatenate([np.ones((x.shape[0],1)), x], axis=1)
-        if self.activation == 'linear':
-            return np.dot(self.X, self.weights)
+        return self.activation.forward(np.dot(self.X, self.weights))
 
-    def backward(self, previous_layer_derivative, learning_rate=1e-2):
-        if self.activation == 'linear':
-            self.weights += np.dot(self.X.T, previous_layer_derivative)/self.X.shape[0]*learning_rate
-            return np.dot(previous_layer_derivative, self.weights[1:, :])/self.X.shape[0]
+    def backward(self, previous_layer_derivative, learning_rate):
+        self.weights += np.dot(self.X.T, previous_layer_derivative*self.activation.backward())/self.X.shape[0]*learning_rate
+        return np.dot(previous_layer_derivative*self.activation.backward(), self.weights[1:, :])/self.X.shape[0]
+            
 
 
 class Network:
@@ -31,14 +51,17 @@ class Network:
             x = layer.forward(x)
         return x
     
-    def backward(self, previous_layer_derivative):
+    def backward(self, previous_layer_derivative, learning_rate):
         for layer in self.layers:
-            previous_layer_derivative = layer.backward(previous_layer_derivative)
+            previous_layer_derivative = layer.backward(previous_layer_derivative, learning_rate)
 
-    def train(self, inputs, y, epochs=1):
-        for i in range(epochs):
+    def train(self, inputs, y, epochs=1, learning_rate=1e-2):
+        lr = learning_rate
+        for i in range(1, epochs+1):
+            if not type(lr) is int and not type(lr) is float:
+                lr = learning_rate(i)
             self.loss.append(np.sum(np.square(y - self.forward(inputs)))/inputs.size)
-            self.backward(2*(y - self.forward(inputs)))
+            self.backward(2*(y - self.forward(inputs)), learning_rate=lr)
 
     def predict(self, X): 
         return self.forward(X)
@@ -49,11 +72,15 @@ class Network:
 
 
 x = np.array([[1], [2], [3], [4], [5]])
-y = np.array([[2], [4], [6], [8], [10]])
+y = np.array([[20], [40], [60], [80], [100]])
 
 m = Network([Layer(1, 1)])
-print(m.layers[0].weights)
-m.train(x, y, 100)
+def lr(x):
+    if x%50 != 0:
+        return 1e-3
+    else: 
+        return 1e-1
+m.train(x, y, 500, learning_rate=lr)
 m.graph_loss()
 plt.plot(x, y)
 plt.plot(x, m.predict(x))
